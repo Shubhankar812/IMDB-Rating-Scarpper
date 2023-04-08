156b7eedd378ec7020758c74546ca22e2bd6a935
Shubhankar812 <shubhankarpande200@gmail.com>
2023-04-08 20:52:42 +0530

    Commiting to Github

--- Scrap.py
+++ Scrap.py
@@ -7,30 +7,29 @@ import openpyxl
 excel = openpyxl.Workbook()
 print(excel.sheetnames)
 sheet=excel.active
-sheet.title='Top Rated Movies'
+sheet.title='Title'
 print(excel.sheetnames)
-sheet.append(['Movie Rank','Movie Name','Year of Release','IMDB Rating'])
+sheet.append(['Article Name','Author Name'])
 
 
 try:
-   source = requests.get('https://www.imdb.com/chart/top')
+   source = requests.get('https://www.theverge.com/23650428/colorblindness-design-ui-accessibility-wordle')
    source.raise_for_status()
 
    soup = BeautifulSoup(source.text,'html.parser')
-   #print(soup.prettify())
-   
+
    movies = soup.find('tbody',class_='lister-list').find_all('tr')
- #  print(len(movies))
+
 
    for mov in movies:
      name = mov.find('td',class_='titleColumn').a.text
      rank = mov.find('td',class_='titleColumn').get_text(strip=True).split('.')[0]
      year = mov.find('td',class_='titleColumn').span.text.strip()
-     rating = mov.find('td',class_="ratingColumn imdbRating").strong.text
-     print(rank,name,year,rating)
-     sheet.append([rank,name,year,rating])
+     element = mov.find('td',class_="duet--article--feature-headline  hover:shadow-underline-inherit").strong.text
+     print(article_name,author_name)
+     sheet.append([article_name,author_name])
 
 except Exception as e:
     print(e)
   
-excel.save('IMBD Movie Rating.xlsx')
+excel.save('data.xlsx')
